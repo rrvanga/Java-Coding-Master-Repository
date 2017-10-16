d7c0816b09d71749aba9d329fd61dd3f658f9a3c
Rajesh <20462886+rrvanga@users.noreply.github.com>
2017-10-16 08:54:57 -0400

    Create SizeOfBinaryTree.java

---
+++ SizeOfBinaryTree.java
@@ -0,0 +1,6 @@
+/* Given a binary tree, write a method to return its size. The size of a tree is the number of nodes it contains. */
+
+public int size(TreeNode root) {
+if(root == null) return 0;
+return size(root.left) + 1 + size(root.right);
+}
